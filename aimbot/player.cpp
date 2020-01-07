6d1f944d4a471b442de59e84449fe8d86c4a8819
Adam <adamczh1@DESKTOP-QI2ARFU.localdomain>
2020-01-07 11:21:33 -0500

    aimbot v2.1

--- aimbot/player.cpp
+++ aimbot/player.cpp
@@ -37,7 +37,8 @@ void Player::handle_new_round(GameState* game_state, RoundState* round_state, in
  */
 void Player::handle_round_over(GameState* game_state, TerminalState* terminal_state, int active)
 {
-    //int my_delta = terminal_state->deltas[active];  // your bankroll change from this round
+    int my_delta = terminal_state->deltas[active];  // your bankroll change from this round
+    //game_state = new GameState(game_state->bankroll + my_delta, game_state->game_clock, game_state->round_num + 1, )->bankroll += my_delta;
     //RoundState* previous_state = (RoundState*) terminal_state->previous_state;  // RoundState before payoffs
     //int street = previous_state->street;  // 0, 3, 4, or 5 representing when this round ended
     //std::array<std::string, 2> my_cards = previous_state->hands[active];  // your cards
@@ -56,23 +57,27 @@ void Player::handle_round_over(GameState* game_state, TerminalState* terminal_st
 Action Player::get_action(GameState* game_state, RoundState* round_state, int active)
 {
     int legal_actions = round_state->legal_actions();  // mask representing the actions you are allowed to take
-    cout << legal_actions << endl;
+    //cout << legal_actions << endl;
     //int street = round_state->street;  // 0, 3, 4, or 5 representing pre-flop, flop, river, or turn respectively
     //std::array<std::string, 2> my_cards = round_state->hands[active];  // your cards
     //std::array<std::string, 5> board_cards = round_state->deck;  // the board cards
-    int my_pip = round_state->pips[active];  // the number of chips you have contributed to the pot this round of betting
+    //int my_pip = round_state->pips[active];  // the number of chips you have contributed to the pot this round of betting
     //int opp_pip = round_state->pips[1-active];  // the number of chips your opponent has contributed to the pot this round of betting
     //int my_stack = round_state->stacks[active];  // the number of chips you have remaining
     //int opp_stack = round_state->stacks[1-active];  // the number of chips your opponent has remaining
     //int continue_cost = opp_pip - my_pip;  // the number of chips needed to stay in the pot
     //int my_contribution = STARTING_STACK - my_stack;  // the number of chips you have contributed to the pot
     //int opp_contribution = STARTING_STACK - opp_stack;  // the number of chips your opponent has contributed to the pot
+    //cout << (game_state->round_num) << ' ' << (game_state->bankroll) << ' ' << (game_state->game_clock) << endl;
+    if ((NUM_ROUNDS - game_state->round_num + 1)*3 < game_state->bankroll) {
+        return FoldAction();
+    }
     if (RAISE_ACTION_TYPE & legal_actions)
     {
         std::array<int, 2> raise_bounds = round_state->raise_bounds();  // the smallest and largest numbers of chips for a legal bet/raise
-        int min_cost = raise_bounds[0] - my_pip;  // the cost of a minimum bet/raise
-        int max_cost = raise_bounds[1] - my_pip;  // the cost of a maximum bet/raise
-        return RaiseAction(max_cost);
+        //int min_cost = raise_bounds[0] - my_pip;  // the cost of a minimum bet/raise
+        //int max_cost = raise_bounds[1] - my_pip;  // the cost of a maximum bet/raise
+        return RaiseAction(raise_bounds[1]);
     }
     if (CHECK_ACTION_TYPE & legal_actions)  // check-call
     {
